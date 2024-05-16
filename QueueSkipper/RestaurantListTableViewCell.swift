d52ff0cb9b611fc132178a402d544c9be3c9a8a8
Keshav Lohiya <keshav.lohiyas@gmail.com>
2024-05-16 17:24:11 +0530

    List

--- QueueSkipper/RestaurantListTableViewCell.swift
+++ QueueSkipper/RestaurantListTableViewCell.swift
@@ -2,25 +2,13 @@
 //  RestaurantListTableViewCell.swift
 //  QueueSkipper
 //
-//  Created by Batch-2 on 15/05/24.
+//  Created by Batch-2 on 16/05/24.
 //
 
 import UIKit
 
 class RestaurantListTableViewCell: UITableViewCell {
 
-    
-    
-    @IBOutlet var restaurantImage: UIImageView!
-    
-    
-    @IBOutlet weak var restaurantName: UILabel!
-    
-    
-    @IBOutlet weak var restaurantWaitingTime: UILabel!
-    
-    
-    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -32,13 +20,4 @@ class RestaurantListTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
-    func updateRestaurants(with restaurant : Restaurant){
-        
-        restaurantImage.image = UIImage(named: "\(restaurant.restImage)")
-        restaurantName.text = restaurant.restName
-        restaurantWaitingTime.text =  "\(restaurant.restWaitingTime) Mins"
-        
-    }
-    
-     
 }
