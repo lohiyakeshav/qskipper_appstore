2ddfbe80b007a4f306c2d4f5b96b7b5f39feefb8
Ayush Yadav <ayush.dms.1415@gmail.com>
2024-05-20 17:00:16 +0530

    Compositional LayOut in Progrss

---
+++ QueueSkipper/RestaurantDetailsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RestaurantDetailsCollectionViewCell.swift
+//  QueueSkipper
+//
+//  Created by Batch-2 on 20/05/24.
+//
+
+import UIKit
+
+class RestaurantDetailsCollectionViewCell: UICollectionViewCell {
+    
+}
