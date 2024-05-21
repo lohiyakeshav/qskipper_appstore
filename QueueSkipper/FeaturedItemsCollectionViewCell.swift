48658fd90eab34b67e95c60d9b3aaf531aa18416
Ayush Yadav <ayush.dms.1415@gmail.com>
2024-05-21 10:16:41 +0530

    Menu

---
+++ QueueSkipper/FeaturedItemsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FeaturedItemsCollectionViewCell.swift
+//  QueueSkipper
+//
+//  Created by ayush yadav on 20/05/24.
+//
+
+import UIKit
+
+class FeaturedItemsCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ QueueSkipper/MenuCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MenuCollectionViewCell.swift
+//  QueueSkipper
+//
+//  Created by ayush yadav on 20/05/24.
+//
+
+import UIKit
+
+class MenuCollectionViewCell: UICollectionViewCell {
+    
+}
